9d2e13c467d8c4613c468483f0a2a87ac8dc4159
jimmymuzza <44672415+jimmymuzza@users.noreply.github.com>
2023-01-16 08:42:01 +0000

    Create Change Azure VPN Encryption settings.ps1

---
+++ Powershell/Change Azure VPN Encryption settings.ps1
@@ -0,0 +1,71 @@
+﻿#Download Azure powershell
+# powershell needs to be version 5.1
+$PSVersionTable.PSVersion
+# allows powersell install through proxy
+[system.net.webrequest]::defaultwebproxy = new-object system.net.webproxy('192.168.0.6:8080')
+[system.net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+[system.net.webrequest]::defaultwebproxy.BypassProxyOnLocal = $true
+
+#Install module
+Install-Module -Name AzureRM -AllowClobber
+Update-Module -Name AzureRM
+
+#Login to Azure
+Connect-AzureRmAccount
+
+#create custom VPN settings https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-ipsecikepolicy-rm-powershell
+$Sub1          = "<YourSubscriptionName>"
+$RG1           = "TestPolicyRG1"
+$Location1     = "East US 2"
+$VNetName1     = "TestVNet1"
+$FESubName1    = "FrontEnd"
+$BESubName1    = "Backend"
+$GWSubName1    = "GatewaySubnet"
+$VNetPrefix11  = "10.11.0.0/16"
+$VNetPrefix12  = "10.12.0.0/16"
+$FESubPrefix1  = "10.11.0.0/24"
+$BESubPrefix1  = "10.12.0.0/24"
+$GWSubPrefix1  = "10.12.255.0/27"
+$DNS1          = "8.8.8.8"
+$GWName1       = "VNet1GW"
+$GW1IPName1    = "VNet1GWIP1"
+$GW1IPconf1    = "gw1ipconf1"
+$Connection16  = "VNet1toSite6"
+
+$LNGName6      = "Site6"
+$LNGPrefix61   = "10.61.0.0/16"
+$LNGPrefix62   = "10.62.0.0/16"
+$LNGIP6        = "131.107.72.22"
+
+#Create the virtual network, VPN gateway, and local network gateway
+$fesub1 = New-AzureRmVirtualNetworkSubnetConfig -Name $FESubName1 -AddressPrefix $FESubPrefix1
+$besub1 = New-AzureRmVirtualNetworkSubnetConfig -Name $BESubName1 -AddressPrefix $BESubPrefix1
+$gwsub1 = New-AzureRmVirtualNetworkSubnetConfig -Name $GWSubName1 -AddressPrefix $GWSubPrefix1
+
+New-AzureRmVirtualNetwork -Name $VNetName1 -ResourceGroupName $RG1 -Location $Location1 -AddressPrefix $VNetPrefix11,$VNetPrefix12 -Subnet $fesub1,$besub1,$gwsub1
+
+$gw1pip1    = New-AzureRmPublicIpAddress -Name $GW1IPName1 -ResourceGroupName $RG1 -Location $Location1 -AllocationMethod Dynamic
+$vnet1      = Get-AzureRmVirtualNetwork -Name $VNetName1 -ResourceGroupName $RG1
+$subnet1    = Get-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $vnet1
+$gw1ipconf1 = New-AzureRmVirtualNetworkGatewayIpConfig -Name $GW1IPconf1 -Subnet $subnet1 -PublicIpAddress $gw1pip1
+
+New-AzureRmVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1 -Location $Location1 -IpConfigurations $gw1ipconf1 -GatewayType Vpn -VpnType RouteBased -GatewaySku VpnGw1
+
+New-AzureRmLocalNetworkGateway -Name $LNGName6 -ResourceGroupName $RG1 -Location $Location1 -GatewayIpAddress $LNGIP6 -AddressPrefix $LNGPrefix61,$LNGPrefix62
+
+#Can jump straight to this to edit an existing connection
+#Create an IPsec/IKE policy
+$ipsecpolicy6 = New-AzureRmIpsecPolicy -IkeEncryption AES256 -IkeIntegrity SHA256 -DhGroup DHGroup24 -IpsecEncryption AES256 -IpsecIntegrity SHA256 -PfsGroup None -SALifeTimeSeconds 14400 -SADataSizeKilobytes 102400000
+
+
+$RG1= "<RESOURCE GROUP>"
+$GWName1= "<Azure-VIRTUAL NETWORK GATEWAY>"
+$LNGName6= ",LOCAL NETWORK GATEWAY NAME>"
+$Connection16= ",VPN NAME>"
+$Location1= "WestEurope"
+
+#Create a S2S VPN connection with an IPsec/IKE policy
+$vnet1gw = Get-AzureRmVirtualNetworkGateway -Name $GWName1  -ResourceGroupName $RG1
+$lng6 = Get-AzureRmLocalNetworkGateway  -Name $LNGName6 -ResourceGroupName $RG1
+
+New-AzureRmVirtualNetworkGatewayConnection -Name $Connection16 -ResourceGroupName $RG1 -VirtualNetworkGateway1 $vnet1gw -LocalNetworkGateway2 $lng6 -Location $Location1 -ConnectionType IPsec -IpsecPolicies $ipsecpolicy6 -SharedKey '$gux#$(keD7k#c6L3{<vPAI'
